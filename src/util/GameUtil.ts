dd33135244ff6a1d41d6718c0750ecc0611458c3
Anthony Lee <hososlch@gmail.com>
2024-01-10 14:23:44 +0800

    refactor: folder structure

---
+++ src/util/GameUtil.ts
@@ -0,0 +1,24 @@
+import {CupOfDices, DiceCall, DiceType} from "./type.ts";
+import {DiceUtil} from "./DiceUtil.ts";
+
+export const GameUtil = {
+    isDiceCountable(dice: DiceType, diceCall: DiceCall): boolean {
+        if (diceCall.pure) {
+            return dice === diceCall.diceType;
+        }
+        return dice === diceCall.diceType || dice === 1;
+    },
+    simulateGameResult(playerCount: number, myCupOfDices: CupOfDices, diceCall: DiceCall): boolean {
+        const otherPlayersDices: CupOfDices[] = [];
+
+        for (let i = 0; i < playerCount - 1; i++) {
+            otherPlayersDices.push(DiceUtil.randomCupOfDices());
+        }
+
+        const myTotalOfSame = myCupOfDices.filter(dice => this.isDiceCountable(dice, diceCall)).length;
+        const otherPlayersTotalOfSame = otherPlayersDices.map(dices => dices.filter(dice => this.isDiceCountable(dice, diceCall)).length);
+        const totalOfSame = myTotalOfSame + otherPlayersTotalOfSame.reduce((acc, cur) => acc + cur, 0);
+
+        return totalOfSame >= diceCall.totalOfSame;
+    },
+};
